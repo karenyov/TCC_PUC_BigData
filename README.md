896971202f33d31b22fbc363a7c22ecea0518033
Karen Vicente <karenyasmin37@gmail.com>
2021-02-22 14:36:48 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # :school: TCC PUC - Ciência de Dados e Big Data :bar_chart: :books:
 PUC Minas - TCC Ciência de Dados e Big Data
 
+## Estrutura do Projeto
+```
+├── Raíz
+│   ├── data
+│   ├── Modelo - TCC
+│   ├── Estudos
+│   ├── Manuais
+├── script.py
+├── TCC - PUC Minas.ipynb
+```
+
+## Requisitos
+- [Python](https://www.python.org/)
+- [Jupyter](https://jupyter.org/) ou uma ferramenta equivalente para execução do arquivo "TCC - PUC Minas.ipynb"
+
+## Iniciando
+Baixar ou clonar (via git) a pasta do projeto deste link [TCC PUC](https://github.com/karenyov/TCC_PUC_BigData).
+
+Excutar o arquivo "script.py" que encontra-se na raíz do projeto
+> Este arquivo é responsável por gerar os datasets necessários. Os arquivos serão salvos na pasta data/.
+
+
+
 
---
+++ script.py
@@ -0,0 +1,72 @@
+#coding: utf-8
+
+import json
+
+from urllib.request import urlopen # Faz a requisição no servidor e obtem a resposta
+import urllib.error
+
+# Definindo variáveis da url de busca
+urlBase = 'https://www.webmotors.com.br/api/';
+urlDetails = urlBase + 'detail/car/'
+
+print('Obtendo os dados, aguarde!');
+
+# Pegando dados
+data = [];
+for i in range(1,40):
+    url = urlBase + 'search/car?url=https://www.webmotors.com.br/carros%2Fsp%3Festadocidade%3DS%25C3%25A3o%2520Paulo%26tipoveiculo%3Dcarros&actualPage='+str(i)
+
+    # Exibir erro caso tenha problemas para obter os dados
+    try:
+        data += json.load(urlopen(url))['SearchResults'];
+        print('Dados da Page: %s obtidos com sucesso, aguarde a criação do arquivo!' % i)
+    except urllib.error.HTTPError as e:
+        print('Erro ao obter dados!' + e)
+    except urllib.error.URLError as e:
+        print('Erro ao obter dados!' + e)
+
+data_fipe = []
+for d in data:
+    # Pega informações detalhadas de cada carro (necessário para obter o valor da tabela FIPE)
+    responseDetails = urlopen(urlDetails + str(d["UniqueId"]))
+    dataDetails = json.load(responseDetails)
+
+    fipe = {}
+    fipe["Fipe"] = 0
+    fipe["UniqueId"] = dataDetails["UniqueId"]
+    
+    if "Specification" in dataDetails and "Evaluation" in dataDetails["Specification"]:
+        fipe["Fipe"] = dataDetails["Specification"]["Evaluation"]["FIPE"]
+        
+    data_fipe.append(fipe)
+    
+    # Parâmetros necessários para categorização
+    # Dados bases necessários para a análise do modelo
+    d["IPVApaid"] = False
+    d["Licensed"] = False
+    d["Warranty"] = False
+    d["OnlyOwner"] = False
+
+    if "Specification" in d and "VehicleAttributes" in d["Specification"]:
+        for atrr in d["Specification"]["VehicleAttributes"]:        
+            if (atrr["Name"] == "IPVA pago"): 
+                d["IPVApaid"] = True
+                
+            if (atrr["Name"] == "Licenciado"): 
+                d["Licensed"] = True
+                  
+            if (atrr["Name"] == "Garantia de fábrica"): 
+                d["Warranty"] = True
+                
+            if (atrr["Name"] == "Único dono"): 
+                d["OnlyOwner"] = True
+
+# Salvando os dados obtidos em um arquivo formato json
+with open('data/data-cars.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
+
+# Salvando os dados FIPE obtidos em um arquivo formato json
+    with open('data/data-cars-fipe.json', 'w', encoding='utf-8') as f:
+        json.dump(data_fipe, f, ensure_ascii=False, indent=4)
+
+print('Dados salvos com sucesso!');
